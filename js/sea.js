14772e89f98923f4019862327c30791d599a34a8
Anton Dziga <antonjyha@gmail.com>
2013-10-02 14:02:25 +0300

    make water blue

--- js/sea.js
+++ js/sea.js
@@ -8,11 +8,9 @@ Sea.prototype = new Sim.Object();
 Sea.prototype.init = function()
 {
     var geometry = new THREE.CubeGeometry(1000, 12, 20);
-    var material = new THREE.MeshNormalMaterial( { color: 0xE53319, transparent: true, opacity: 0.7 } );
-    // 0x0066FF
+    var material = new THREE.LineBasicMaterial( { color: 0x0066FF, transparent: true, opacity: 0.7 } );
     var mesh = new THREE.Mesh( geometry, material ); 
     mesh.position.y = -6;
-
-    // Tell the framework about our object
+    
     this.setObject3D(mesh);    
 }
